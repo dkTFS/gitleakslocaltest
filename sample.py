8bad444f99f657d2a17eaf8ed15a7b3049eff6e9
Declan Kneita <declan.kneita@toyota.com>
2023-06-29 13:40:08 -0500

    local gitleaks test

---
+++ sample.py
@@ -0,0 +1,20 @@
+#some algorithm generated generic API keys, GitLeaks should pick these up
+
+key = "01bf9eea-adrz-7140-ryd1-221383c116ed"
+
+
+generic64 = "wO34F8tSNoyfnHJatL8L7SYXubopkSJL1PqF6yddHzlx6Rh21iCLERe0eJYxXTJi"
+generic128 = "MzHQV6jQN2OlL5OwilDcGkpfJVH46rVNOCVhMYvMlBj69icATZJROR8YPVRdiyZH1UjhgpmWZvE7783d0ULjjLcmmpG1ZwiPN707KAUQsWflRctnsdabCtM27p5UxABV"
+generic256 = "e7ZNWN3bqU73zlq2Qo8jb0KuU4UcqNn1blshfnYn41ddqZsouBTRYHufWRdqVNCF0pYmpLa29inSMkjuKI4JssWlkKGVkQo1JzACwiq8yHbui0YJLX90GEblTZfWCFBZhJfxXrUFPL56MxLWFzI6PBJd8f0egk8oRxWuSjbHHhI9q7Ih30Jnm5EDy7S5fjoZtX4r4D5szgugtog48xVgJ6ZjC98QDz2geW1MewOO2SEU63eIdUU54DbNpF4sLBcL"
+
+#a bad and good password, I am not sure if gitleaks will pick these up. I do no think it will though as the regexes it used are designed to pick up API key patterns.
+#it most likely does not have regexes to find passwords and emails because the regex would have to be extremely general, which would result in many false positives
+
+human_password = "silly_password14"
+algorithm_password = "$uV&4F+d#G2Jyqzr"
+
+#an email, I do not expect gitleaks to pick this one up either
+email = "blabla@gmail.com"
+
+
+
